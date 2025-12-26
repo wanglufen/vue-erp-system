995974ea7ec3b413f9481ba94b0f2c2a558bebb2
wanglufen <1957612146@qq.com>
2025-12-26 08:55:49 +0800

    Delete src directory

--- src/api/mock/userData.ts
+++
@@ -1,5 +0,0 @@
-// 模拟数据库里的用户表
-export const MOCK_USERS = [
-  { id: 1, username: 'admin', password: '123', name: '超级管理员', role: 'admin' },
-  { id: 2, username: 'user', password: '123', name: '普通员工', role: 'user' }
-]
